f0693c464cdb2e8ff6f29a035132de66b58da247
matinDesai <matindesai7774@gmail.com>
2024-06-25 22:13:45 +0530

    this is my first commit

---
+++ src/k/c1.java
@@ -0,0 +1,32 @@
+package k;
+
+public class c1 {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("m");
+	}
+	
+	
+	
+	public void m1() {
+		
+		
+		System.out.println("my name is matin");
+	}
+	
+	
+	public void m2() {
+		
+		System.out.println("my name is desai");
+	}
+	
+	
+	public void m3() {
+		
+		System.out.println("my name akashy");
+	}
+
+}
